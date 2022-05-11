92ce5a7664f39fdfc4825ccb1cbb09c4aa66c2da
tikoabelyan <tikoabelyan1995@gmail.com>
2022-05-11 11:38:56 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const arr = new MyArray();
+arr.push("first");
+arr.push("second");
+arr.push("third");
+arr.delete(1);
+console.log(arr);
