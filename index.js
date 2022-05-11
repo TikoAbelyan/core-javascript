083169f9762165880200ac8631608def9ee82053
tikoabelyan <tikoabelyan1995@gmail.com>
2022-05-11 11:58:48 +0400

    prototype Initializing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//javascript class start
 class MyArray {
   constructor() {
     this.length = 0;
@@ -36,3 +37,46 @@ arr.push("second");
 arr.push("third");
 arr.delete(1);
 console.log(arr);
+
+//javascript class end
+
+// Initializing a constructor function start
+function fn() {
+  this.data = {};
+  this.length = 0;
+}
+
+fn.prototype.customPush = function (e) {
+  this.data[this.length] = e;
+  this.length++;
+  return this.length;
+};
+
+fn.prototype.get = function (index) {
+  return this.data[index];
+};
+
+fn.prototype.pop = function () {
+  const lastItem = this.data[this.length - 1];
+  delete this.data[this.length - 1];
+  this.length--;
+  return lastItem;
+};
+
+fn.prototype.delete = function (index) {
+  for (let i = index; i < this.length - 1; i++) {
+    this.data[i] = this.data[i + 1];
+  }
+  delete this.data[this.length - 1];
+  this.length--;
+};
+
+const array = new fn();
+
+array.customPush("Hi");
+array.customPush("fine");
+array.delete(0);
+
+console.log(array);
+
+// Initializing a constructor function end
